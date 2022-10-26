27e2e365d15dcd53313652090dae89e8d1170c81
SuroZaqaryan <suren.zakaryan177@gmail.com>
2022-10-26 04:02:25 +0300

    Init Vuex

--- src/main.js
+++ src/main.js
@@ -1,31 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import store from './store'
 import router from './router'
 
 Vue.config.productionTip = false
 
 new Vue({
+  store,
   router,
   render: h => h(App),
 }).$mount('#app')
-
-
-
-// import Vue from 'vue'
-// import App from './App'
-// import router from './router'
-// import VueTheMask from 'vue-the-mask'
-// import VeeValidate from 'vee-validate'
-// import Meta from 'vue-meta'
-
-// Vue.config.productionTip = false
-// Vue.use(VueTheMask)
-// Vue.use(VeeValidate)
-// Vue.use(Meta)
-
-// /* eslint-disable no-new */
-// new Vue({
-//   el: '#app',
-//   router,
-//   render: h => h(App)
-// })
